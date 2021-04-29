12391d824cad44917e05e26531cd9cdc0d192bfb
Yasser Alattas <38978501+yalattas@users.noreply.github.com>
2021-04-29 14:58:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Practice
+ Password Generator and more
---
+++ app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import random
+# Create your views here.
+
+def header(request):
+    return render(request, 'main/header.html')
+def home(request):
+    return render(request, 'subpages/index.html')
+def generatedPassword(request):
+    passwordResult = ''
+    # Default is 12 characters in case user didn't choose
+    length = int(request.GET.get('length', 12))
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('upper_case'):
+        characters.extend('ABCDEFGHIJKLMNOPQRSTUVWZYX')
+    if request.GET.get('number_case'):
+        characters.extend('123456789')
+    if request.GET.get('special_char'):
+        characters.extend('@#$%')
+    for x in range(length):
+        passwordResult += random.choice(characters)
+
+    return render(request, 'subpages/pass.html', {'password': passwordResult})
