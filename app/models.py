e4b6662ef18365f873c1f54c445a85a563a5b0c1
Yasser Alattas <38978501+yalattas@users.noreply.github.com>
2021-05-05 21:38:09 +0300

    Continuation

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 import datetime
 
 # Create your models here.
@@ -21,6 +23,22 @@ class Visitor (models.Model):
     def __str__(self):
         return self.email + '-' + str(self.id)
 
+# Require import from Django auth #from django.contrib.auth.models import AbstractUser
+'''class CustomUser(User):
+    created_at = models.DateTimeField(default=datetime.datetime.now())
+    modified_at = models.DateTimeField(default=datetime.datetime.now())
+    user_name =  models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    user = models.CharField(
+        ('user'), unique=True, max_length=85
+    )
+    email_address = models.EmailField(('email address'), unique=True)
+    class Meta:
+        ordering = ('user','email address','password')
+
+    def __str__(self):
+        return self.user
+'''
 class User(models.Model):
     created_at = models.DateTimeField(default=datetime.datetime.now())
     modified_at = models.DateTimeField(default=datetime.datetime.now())
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 def blog_home(request):
     # To sort it based on most recent date
-    posts = Post.objects.order_by('created_at')
+    posts = Post.objects.order_by('-created_at') #[:3] #Get the first 3 posts
     # posts is a directory coming from above variable passing to view
     return render(request, 'blog/all_home.html', {'posts':posts})
 def details(request, blog_id):
